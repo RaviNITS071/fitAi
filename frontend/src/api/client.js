38af65bfdc01b295ffd48a0608fe6b867c690909
RAVI <ravinits071@gmail.comgit config --global user.email ravinits071@gmail.com>
2026-06-03 03:12:57 +0530

    fix: Uncomment production API URL and comment out development URL for deployment

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 // Development URL (Use this for local testing in VS Code)
-const API_URL = "http://localhost:5000/api";
+//const API_URL = "http://localhost:5000/api";
 
 // Production URL (Uncomment this jab aap naya backend Render par deploy kar do)
-// const API_URL = "https://fitai-backend-8v70.onrender.com/api";
+ const API_URL = "https://fitai-backend-8v70.onrender.com/api";
 
 export const registerUser = (username, email, password) => axios.post(`${API_URL}/register`, { username, email, password });
 export const loginUser = (email, password) => axios.post(`${API_URL}/login`, { email, password });
